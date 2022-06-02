c5e504d0248d1cda4bdb133c0b32076e976efed9
Jared Kelly <jpkell046@gmail.com>
2022-06-02 09:37:37 -0700

    Update test.js

--- codeWar/test.js
+++ codeWar/test.js
@@ -1,1 +1,20 @@
-console.log('hellow')
+function greet(name){
+  if (name === 'Johnny'){
+    return "Hello, my love!";
+  } else {
+     return "Hello, " + name + "!";
+  }
+  }
+  
+  function makeNegative(num) {
+    return num < 0 ? num : -num;
+  }  
+
+ /* function makeNegative(num) {
+    if ( num > 0 ) {
+          return -num;
+      }
+      else {
+          return num;
+      }
+  } */
