18af29280c565d79310d41ac444c1c55e0aeb7d2
Jared Kelly <jpkell046@gmail.com>
2022-06-14 12:34:49 -0700

    Update test.js

--- codeWar/test.js
+++ codeWar/test.js
@@ -100,3 +100,11 @@ function grow(x){
 
 //or 
 const greet= () =>"hello world!";
+
+function areYouPlayingBanjo(name) {
+  // Implement me
+  if (name[0] == 'R' || name[0] == 'r')
+    return name + " plays banjo";
+  else
+    return name + " does not play banjo";
+}
