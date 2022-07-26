52afa19f1030de3e897f84df212e49f6e7ae7bdf
Jared Kelly <jpkell046@gmail.com>
2022-07-26 11:44:15 -0700

    Create rerun.js

---
+++ codeWar/rerun.js
@@ -0,0 +1,40 @@
+//was missing return
+function multiply(a, b){
+    return a * b
+  }
+  
+//was missing break;
+  function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+        break;
+      case 2:
+        name = 'Venus'
+        break;
+      case 3:
+        name = 'Earth'
+        break;
+      case 4:
+        name = 'Mars'
+        break;
+      case 5:
+        name = 'Jupiter'
+        break;
+      case 6:
+        name = 'Saturn'
+        break;
+      case 7:
+        name = 'Uranus'
+        break;
+      case 8:
+        name = 'Neptune'
+    }
+    return name;
+}
+
+//
+function solution(str){
+    return str.split('').reverse().join('');  
+  }
