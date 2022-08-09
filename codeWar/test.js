0965d2d7fd23b3b47ffc8e4e1325ddb1fe6d6a7b
Jared Kelly <jpkell046@gmail.com>
2022-08-09 11:33:37 -0700

    Update test.js

--- codeWar/test.js
+++ codeWar/test.js
@@ -156,5 +156,12 @@ function isDivisible(n, x, y) {
   return ( n% x== 0 && n%y ==0) ? true : false;
 }
 
-/TEST PU BLISH
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
+function makeNegative(num) {
+  if (num >= 0){
+    return num *-1
+  }else{
+    return num 
+  }
+}
