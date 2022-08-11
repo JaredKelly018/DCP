d065b8f3f6fa6d98259333ee81408916dc396841
Jared Kelly <jpkell046@gmail.com>
2022-08-11 14:03:06 -0700

    Update test.js

--- codeWar/test.js
+++ codeWar/test.js
@@ -164,4 +164,16 @@ function makeNegative(num) {
   }else{
     return num 
   }
+  //  return num < 0 ? num : -num; this would b better
+}
+
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+  
+  return result;
 }
