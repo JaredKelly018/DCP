c05ad8584190ec5eecd08ebe519cae520365c15b
Jared Kelly <jpkell046@gmail.com>
2022-08-23 07:20:21 -0700

    Update test.js

--- codeWar/test.js
+++ codeWar/test.js
@@ -219,3 +219,21 @@ function positiveSum(arr) {
   }
   return total;                         // return total
 }   \
+
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+  if( operation == "+") {
+    return value1 + value2
+  }
+  if( operation == "-") {
+    return value1 - value2
+  }
+  if( operation == "*") {
+    return value1 * value2
+  }
+  if( operation == "/") {
+    return value1 / value2
+  }
+}
